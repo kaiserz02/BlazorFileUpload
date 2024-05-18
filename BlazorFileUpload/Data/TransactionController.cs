26bc9bad6adc1ba643386d1f16eff3522fbbc570
Kim de Silva <kdesilva@1080agile.com>
2024-05-19 02:17:17 +0800

    Implemeted GetBySymbol to transaction page

---
+++ BlazorFileUpload/Data/TransactionController.cs
@@ -0,0 +1,35 @@
+﻿using BlazorFileUpload.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorFileUpload.Data
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public TransactionController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetItemsBySymbol")]
+        public async Task<ActionResult<List<Transaction>>> GetItemsBySymbol(string symbol) {
+            var transactions = await _context.Transactions
+                .Where(t => t.Symbol == symbol)
+                .ToListAsync();
+
+            if (transactions.Any())
+            {
+                return Ok(transactions);
+            }
+            else
+            {
+                return NotFound(new { StatusCode = 404, Message = "No data found for symbol: " + symbol });
+            }
+        }
+    }
+}
---
+++ BlazorFileUpload/Services/TransactionFileService.cs
@@ -0,0 +1,27 @@
+﻿using BlazorFileUpload.Model;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.Forms;
+namespace BlazorFileUpload.Services
+{
+    public class TransactionFileService
+    {
+        private readonly HttpClient _httpClient;
+        public TransactionFileService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Transaction>> GetItemsBySymbol(string symbol) {
+            try
+            {
+                return await _httpClient.GetFromJsonAsync<List<Transaction>>($"api/transaction/GetItemsBySymbol?symbol={symbol}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error occurred while fetching transactions by symbol: {ex.Message}");
+                return new List<Transaction>();
+            }
+        }
+
+    }
+}
